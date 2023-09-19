32332a56562c2744cb64a3afde050648b09b9b73
Rajasofia1011 <rajasofia0123@gmail.com>
2023-09-19 16:59:35 +0530

    first commit

--- A.java
+++ A.java
@@ -3,5 +3,7 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("i am into lara");
+		System.out.println("i am into lara");
+		System.out.println("i am into lara");
 	}
 }
